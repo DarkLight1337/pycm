e6700127431f5d81f081be198bfe8564269573b8
DarkLight1337 <cyrus.tl.leung@gmail.com>
2024-06-05 12:26:30 +0800

    Fix CLI test for windows

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,6 +1,7 @@
 import subprocess
+import sys
 
 
 def test_help():
     assert subprocess.call(['python', '-m', 'pycm', '--help']) == 0
-    assert subprocess.call(['pycm', '--help']) == 0
+    assert subprocess.call(['pycm', '--help'], shell=sys.platform == "win32") == 0
